4c46e6ecfcbc7cf77a6239477e303052ec607096
Bhawna Saroha <30bhawna95@gmail.com>
2018-02-09 20:10:17 +0530

    Added files to the repo

---
+++ script.js
@@ -0,0 +1,168 @@
+var myData = {
+  "quotes": [
+    ["Driver picks the music, shotgun shuts his cake hole.", "Dean"],
+    ["It must be hard with your sense of direction, never being able to find your way to a decent pickup line.", "Andrea"],
+    ["Dude, stow the touchy-feely, self-help yoga crap.", "Dean"],
+    ["Why’d you let me fall asleep?", "Sam"],
+    ["Because I’m an awesome brother. So what did you dream about?", "Dean"],
+    ["Lollipops and candy canes.", "Sam"],
+    ["Your half-caf, double vanilla latte is getting cold over here, Francis.", "Dean"],
+    ["Boy, you put your foot on my coffee table, I’m gonna whack you with a spoon.", "Missouri"],
+    ["Who do you think is a hotter psychic: Patricia Arquette, Jennifer Love Hewitt or you?", "Dean"],
+    ["I had a crappy guidance counselor.", "Sam"],
+    ["The powerful have always preyed on the powerless. That's how they became powerful in the first place.", "Tyrion Lannister"],
+    ["If you ever call me 'sister' again, I'll have you strangled in your sleep.", "Cersei Lannister"],
+    ["Stick 'em with the pointy end.", "Jon Snow"],
+    ["Let me give you some advice, bastard. Never forget what you are. The rest of the world will not. Wear it like armor, and it can never be used to hurt you.", "Tyrion Lannister"],
+    ["That's what I do. I drink and I know things.", "Tyrion Lannister"],
+    ["A lion doesn't concern himself with the opinions of a sheep.", "Tywin Lannister"],
+    ["Power resides where men believe it resides. It's a trick. A shadow on the wall. And a very small man can cast a very large shadow.", "Lord Varys"],
+    ["The next time you raise a hand to me will be the last time you have hands.", "Daenerys Targaryen"],
+    ["It's easy to confuse 'what is' with 'what ought to be', especially when 'what is' has worked out in your favor.", "Tyrion Lannister"],
+    ["What happened down in the dungeons between you and Professor Quirrell is a complete secret, so, naturally, the whole school knows.", "Albus Dumbledore"],
+    ["The truth. It is a beautiful and terrible thing, and should therefore be treated with great caution.", "Albus Dumbledore"],
+    ["Too much! Too much is wrong with me. That’s the problem isn’t it? Too much is wrong with me, and you can’t do anything about that. You can’t change it. You can’t fix me. Because I’m not broken, I don’t need to be fixed, OK? I’m me!", "Ian Gallaghar"],
+    ["She's so mysterious. She's like the sphinx. Part-cat, part whatever-the-fuck.", "Kevin Ball"],
+    ["Gentlemen, you can't fight in here! This is the War Room.", "Dr. Strangelove"],
+    ["That's it, Dishonor! Dishonor on your whole family! Dishonor on you, dishonor on your cow!", "Mushu"],
+    ["By all means move at a glacial pace. You know how that thrills me.", "Miranda Priestley"],
+    ["I can't dance, I can't dance, I've got two left feet!", "Gerry Fleck"],
+    ["Get out of the way, son. You're using my oxygen.", "McMurphy"],
+    ["I'm not bad, I'm just drawn that way.", "Jessica Rabbit"],
+    ["If you want pain, try wearing a corset!", "Elizabeth Swann"],
+    ["You know Jack, I thought I had you figured out. But it turns out you're a hard man to predict.", "Captain Barbossa"],
+    ["Me? I'm dishonest, and a dishonest man you can always trust to be dishonest. Honestly. It's the honest ones you want to watch out for, because you never know when they're going to do something incredibly... stupid.", "Captain Jack Sparrow"],
+    ["Gentlemen! You shall always remember this as the day that you almost caught Captain Jack Sparrow!", "Captain Jack Sparrow"],
+    ["You better start believing in ghost stories Miss Turner, you're in one!", "Captain Barbossa"],
+    ["You are, without doubt, the worst pirate I've ever heard of.", "Norrington"],
+    ["I found a liquor store and I drank it.", "Castiel"],
+    ["Please accept this sandwich as a gesture of solidarity.", "Castiel"],
+    ["Why is six afraid of seven? I assume because seven is a prime number, and prime numbers can be intimidating.", "Castiel"],
+    ["So the Hardy boys finally found me. Took you long enough.", "Crowley"],
+    ["I can't, and I meant I can't, you mop-headed lumberjack!", "Crowley"],
+    ["Submit or die? What are you, French?", "Crowley"],
+    ["Call on the bat-phone? Never call on business hours, do they?", "Crowley"],
+    ["Never underestimate the King of Hell, darling. I know a lot of swell tricks.", "Crowley"],
+    ["If debugging is the process of removing software bugs, then programming must be the process of putting them in.", "Edsger Dijkstra"],
+    ["I don’t care if it works on your machine! We are not shipping your machine!", "Vidiu Platon"],
+    ["Measuring programming progress by lines of code is like measuring aircraft building progress by weight.", "Bill Gates"],
+    ["One man’s crappy software is another man’s full time job.", "Jessica Gaston"],
+    ["Programming is like sex. One mistake and you have to support it for the rest of your life.", "Michael Sinz"],
+    ["Talk is cheap. Show me the code.", "Linus Torvalds"],
+    ["Software is like sex: It’s better when it’s free.", "Linus Torvalds"],
+    ["Any code of your own that you haven’t looked at for six or more months might as well have been written by someone else.", "Eagleson’s Law"],
+    ["If you think math is hard, try web design.", "Trish Parr"],
+    ["There are three responses to a piece of design – yes, no, and WOW! Wow is the one to aim for.", "Milton Glaser"],
+    ["What separates design from art is that design is meant to be… functional.", "Cameron Moll"],
+    ["Websites promote you 24/7: No employee will do that.", "Paul Cookson"],
+    ["Intuitive design is how we give the user new superpowers.", "Jared Spool"],
+    ["Create your own visual style… let it be unique for yourself and yet identifiable for others.", "Orson Welles"],
+    ["Technology over technique produces emotionless design.", "Daniel Mall"],
+    ["Great web design without functionality is like a sports car with no engine.", "Paul Cookson"],
+    ["Good design is obvious. Great design is transparent.", "Joe Sparano"],
+    ["Without requirements or design, programming is the art of adding bugs to an empty text file.", "Louis Srygley"],
+    ["Before software can be reusable it first has to be usable.", "Ralph Johnson"],
+    ["There are two ways to write error-free programs; only the third one works.", "Alan J. Perlis"],
+    ["Deleted code is debugged code.", "Jeff Sickel"],
+    ["There are only two kinds of programming languages: those people always bitch about and those nobody uses.", "Bjarne Stroustrup"]
+  ],
+  "backgroundColors": [
+    "linear-gradient(to right, #ff5501, #ff036d)",
+    "linear-gradient(to right, #0131ff, #8803ff)",
+    "linear-gradient(to right, #0003ff, #00ffa3)",
+    "linear-gradient(to right, #ff2e00, #ffc700)",
+    "linear-gradient(to right, #e9147a, #0b2bb3)",
+    "linear-gradient(to right, #8247af, #e04631)",
+    "linear-gradient(to right, #09e04c, #575757)",
+    "linear-gradient(to right, #fe5b00, #2300f7)",
+    "linear-gradient(to right, #0f0e0c, #b11e4a)",
+    "linear-gradient(to right, #1674df, #d114c4)",
+    "linear-gradient(to right, #2aaf7b, #e93711)",
+    "linear-gradient(to right, #261c8c, #3e8b28)",
+    "linear-gradient(to right, #0ebad9, #e24911)"
+  ]
+};
+
+$(document).ready(function(){
+  var random;
+  $("#nextQuote").click(function(){
+    var background;
+    random = Math.floor(Math.random() * (myData.quotes.length + 1));
+
+    if(random <= 5){
+      document.getElementById("theQuote").innerHTML = myData.quotes[random][0];
+      document.getElementById("quotedBy").innerHTML = myData.quotes[random][1];
+      background = myData.backgroundColors[0];
+      document.getElementById("body").style.background = background;
+    } else if(random <= 10){
+      document.getElementById("theQuote").innerHTML = myData.quotes[random][0];
+      document.getElementById("quotedBy").innerHTML = myData.quotes[random][1];
+      background = myData.backgroundColors[1];
+      document.getElementById("body").style.background = background;
+    } else if(random <= 15){
+      document.getElementById("theQuote").innerHTML = myData.quotes[random][0];
+      document.getElementById("quotedBy").innerHTML = myData.quotes[random][1];
+      background = myData.backgroundColors[2];
+      document.getElementById("body").style.background = background;
+    } else if(random <= 20){
+      document.getElementById("theQuote").innerHTML = myData.quotes[random][0];
+      document.getElementById("quotedBy").innerHTML = myData.quotes[random][1];
+      background = myData.backgroundColors[3];
+      document.getElementById("body").style.background = background;
+    } else if(random <= 25){
+      document.getElementById("theQuote").innerHTML = myData.quotes[random][0];
+      document.getElementById("quotedBy").innerHTML = myData.quotes[random][1];
+      background = myData.backgroundColors[4];
+      document.getElementById("body").style.background = background;
+    } else if(random <= 30){
+      document.getElementById("theQuote").innerHTML = myData.quotes[random][0];
+      document.getElementById("quotedBy").innerHTML = myData.quotes[random][1];
+      background = myData.backgroundColors[5];
+      document.getElementById("body").style.background = background;
+    } else if(random <= 35){
+      document.getElementById("theQuote").innerHTML = myData.quotes[random][0];
+      document.getElementById("quotedBy").innerHTML = myData.quotes[random][1];
+      background = myData.backgroundColors[6];
+      document.getElementById("body").style.background = background;
+    } else if(random <= 40){
+      document.getElementById("theQuote").innerHTML = myData.quotes[random][0];
+      document.getElementById("quotedBy").innerHTML = myData.quotes[random][1];
+      background = myData.backgroundColors[7];
+      document.getElementById("body").style.background = background;
+    } else if(random <= 45){
+      document.getElementById("theQuote").innerHTML = myData.quotes[random][0];
+      document.getElementById("quotedBy").innerHTML = myData.quotes[random][1];
+      background = myData.backgroundColors[8];
+      document.getElementById("body").style.background = background;
+    } else if(random <= 50){
+      document.getElementById("theQuote").innerHTML = myData.quotes[random][0];
+      document.getElementById("quotedBy").innerHTML = myData.quotes[random][1];
+      background = myData.backgroundColors[9];
+      document.getElementById("body").style.background = background;
+    } else if(random <= 55){
+      document.getElementById("theQuote").innerHTML = myData.quotes[random][0];
+      document.getElementById("quotedBy").innerHTML = myData.quotes[random][1];
+      background = myData.backgroundColors[10];
+      document.getElementById("body").style.background = background;
+    } else if(random <= 60){
+      document.getElementById("theQuote").innerHTML = myData.quotes[random][0];
+      document.getElementById("quotedBy").innerHTML = myData.quotes[random][1];
+      background = myData.backgroundColors[11];
+      document.getElementById("body").style.background = background;
+    } else {
+      document.getElementById("theQuote").innerHTML = myData.quotes[random][0];
+      document.getElementById("quotedBy").innerHTML = myData.quotes[random][1];
+      background = myData.backgroundColors[12];
+      document.getElementById("body").style.background = background;
+    }
+    $("#twitter-button").attr("href","https://twitter.com/intent/tweet?text=" + myData.quotes[random][0] +"&hashtags=" + myData.quotes[random][1]);
+  });
+  $("#nextQuote").trigger("click");
+
+  });
+
+// "https://twitter.com/intent/tweet?button_hashtag=TwitterStories&hashtags=sun,water"
+
+    // $("#tweet").on("click", function(){
+    //  window.open("https://twitter.com/intent/tweet?text=" + randomQuote);
+    //   });
